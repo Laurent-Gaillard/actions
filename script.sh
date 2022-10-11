6121e85cbc527821f05bcf79947eda76f59aa1d5
Hubert Quarantel-Colombani <hubert@quarantel.name>
2022-10-11 18:00:26 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,17 +7,19 @@ mkdir -p ${WORKDIR}/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 
 RC=0
 
-[ -n "${INPUT_PROVIDED_VERSION}" ] && PROVIDED_VERSION_OPTION="-D \"provided_version ${INPUT_PROVIDED_VERSION}\""
-[ -n "${INPUT_PROVIDED_RELEASE}" ] && PROVIDED_RELEASE_OPTION="-D \"provided_release ${INPUT_PROVIDED_RELEASE}\""
+RPMMACROS_OPTIONS="-D '_topdir ${WORKDIR}/rpmbuild'"
+
+[ -n "${INPUT_PROVIDED_VERSION}" ] && RPMMACROS_OPTIONS="${RPMMACROS_OPTIONS} -D 'provided_version ${INPUT_PROVIDED_VERSION}'"
+[ -n "${INPUT_PROVIDED_RELEASE}" ] && RPMMACROS_OPTIONS="${RPMMACROS_OPTIONS} -D 'provided_release ${INPUT_PROVIDED_RELEASE}'"
 
 cp ${INPUT_SELINUX_FILES_LOCATION}/{*.te,*.fc,*.if} ${WORKDIR}/rpmbuild/SOURCES/
 RC=$(($RC + $? ))
 cp ${INPUT_SPEC_FILE_LOCATION}/*.spec ${WORKDIR}/rpmbuild/SPECS/
 RC=$(($RC + $? ))
 
-for specfile in $( find ${WORKDIR}/rpmbuild/SPECS/ -type f)
+for specfile in $( find ${WORKDIR}/rpmbuild/SPECS/ -type f )
 do
-  /usr/bin/rpmbuild -bb -D "_topdir ${WORKDIR}/rpmbuild" ${PROVIDED_VERSION_OPTION} ${PROVIDED_RELEASE_OPTION} ${specfile}
+  /usr/bin/rpmbuild -bb ${RPMMACROS_OPTIONS} ${specfile}
   RC=$(($RC + $? ))
 done
 
