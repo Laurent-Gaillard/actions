57726399f4ca2ad40f959b345b02ae94d92b4f27
Laurent Gaillard <lgaillard01@gmail.com>
2022-10-14 12:04:16 +0200

    adding contraints on selinux-policy version at build time
    Signed-off-by: Laurent Gaillard <lgaillard01@gmail.com>

--- script.sh
+++ script.sh
@@ -21,19 +21,28 @@ then
   then
     echo "::notice title=RPMbuild::Source repository directory is ${WORKDIR}/${INPUT_SOURCE_REPO_LOCATION}"
 
-    for specfile in ${WORKDIR}/${INPUT_SOURCE_REPO_LOCATION}/${INPUT_SPEC_FILE_LOCATION}/*.spec
-    do
-      /usr/bin/rpmbuild -bb \
-        --define="_topdir ${WORKDIR}/rpmbuild" \
-        --define="_builddir ${WORKDIR}/${INPUT_SOURCE_REPO_LOCATION}" \
-        --define="provided_version ${INPUT_PROVIDED_VERSION:-null}" \
-        --define="provided_release ${INPUT_PROVIDED_RELEASE:-null}" \
-        ${specfile}
-      rc=$?
-      [ $rc -ne 0 ] && echo "::error title=RPMbuild::Could not build RPM for spec file ${specfile}."
-      
-      RC=$(($RC + $rc ))
-    done
+    declare $(rpm -q --qf "%{V}\n" selinux-policy | awk -F '.' '{ major=$1; minor=$2 } END { if( major*minor==0) {print "_sepol_minver_cond=0" ; print "_sepol_maxver_cond=0"} else {print "_sepol_minver_cond="major"."minor; print "_sepol_maxver_cond="major"."(minor+1)}}')
+
+    if [ "${_sepol_minver_cond}" != "0" -a "${_sepol_maxver_cond}" != "0" ]
+    then
+      for specfile in ${WORKDIR}/${INPUT_SOURCE_REPO_LOCATION}/${INPUT_SPEC_FILE_LOCATION}/*.spec
+      do
+        /usr/bin/rpmbuild -bb \
+          --define="_topdir ${WORKDIR}/rpmbuild" \
+          --define="_builddir ${WORKDIR}/${INPUT_SOURCE_REPO_LOCATION}" \
+          --define="_provided_version ${INPUT_PROVIDED_VERSION:-null}" \
+          --define="_provided_release ${INPUT_PROVIDED_RELEASE:-null}" \
+          --define="_sepol_minver_cond >= ${_sepol_minver_cond}" \
+          --define="_sepol_maxver_cond <= ${_sepol_maxver_cond}" \
+          ${specfile}
+        rc=$?
+        [ $rc -ne 0 ] && echo "::error title=RPMbuild::Could not build RPM for spec file ${specfile}."
+        
+        RC=$(($RC + $rc ))
+      done
+    else
+      echo "::error title=RPMbuild::Could not define selinux-policy version requirements"
+    fi      
   else
     echo "::error title=RPMbuild::Could not find RPM spec file in ${WORKDIR}/${INPUT_SOURCE_REPO_LOCATION}/${INPUT_SPEC_FILE_LOCATION}/"
   fi
